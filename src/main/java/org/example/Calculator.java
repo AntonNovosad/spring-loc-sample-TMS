4bc81f2bad3693c0b2879cd4ce187ffe612f5b8a
Anton Novosad <noobito.2011@gmail.com>
2021-05-12 10:33:08 +0300

    added class Calculator

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+
+public class Calculator {
+    private Scanner scanner = new Scanner(System.in);
+    private boolean isStarted = true;
+
+
+    public void run() {
+        while (isStarted) {
+            write("Select operation");
+            Operation operation = select();
+            operation.action();
+        }
+        write("Goodbye");
+    }
+
+    private void write(String message) {
+        System.out.println(message);
+    }
+
+    private Operation select() {
+        switch (scanner.nextInt()) {
+            case 1:
+                return new Addition();
+            case 2:
+                return new Subtraction();
+            case 3:
+                return new Multiply();
+            case 4:
+                return new Divide();
+            case 0:
+                isStarted = false;
+        }
+        return null;
+    }
+}
