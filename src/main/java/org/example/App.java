4d4fbb14527992a4e40b9b7aef0a726d0a6e1f7a
Anton Novosad <noobito.2011@gmail.com>
2021-05-12 11:31:30 +0300

    change create object calculator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfiguration.class);
-        Calculator calculator = (Calculator) applicationContext.getBean("calculator");
+        Calculator calculator = applicationContext.getBean(Calculator.class);
         calculator.run();
     }
 }
