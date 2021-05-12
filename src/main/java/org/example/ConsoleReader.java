2ba6598e4473b1f90e50ffc7b4c59b22bb2018e4
Anton Novosad <noobito.2011@gmail.com>
2021-05-12 10:32:55 +0300

    added ConsoleReader and ConsoleWriter

---
+++ src/main/java/org/example/ConsoleReader.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ConsoleReader {
+
+    public static void reader(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/org/example/ConsoleWriter.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ConsoleWriter {
+    public static int numberWriter() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
