3faf8c8d865eb63c773c14eb95878277d5d07afb
Anton Novosad <noobito.2011@gmail.com>
2021-06-13 21:52:52 +0300

    added beans in RootConfiguration

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.example.calculator.Calculator;
+import org.example.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfiguration.class);
-        Calculator calculator = applicationContext.getBean(Calculator.class);
-        calculator.run();
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.run();
     }
 }
--- src/main/java/org/example/RootConfiguration.java
+++ src/main/java/org/example/RootConfiguration.java
@@ -1,16 +1,98 @@
 package org.example;
 
-import org.example.calculator.Calculator;
+import org.example.dao.HistoryOperationDao;
+import org.example.dao.UserDao;
+import org.example.dao.inMemoryDao.InMemoryHistoryOperationDao;
+import org.example.dao.inMemoryDao.InMemoryUserDao;
+import org.example.entity.User;
+import org.example.entity.calculator.*;
+import org.example.entity.command.Authorization;
+import org.example.entity.command.Command;
+import org.example.entity.command.Registration;
+import org.example.service.CalculatorService;
+import org.example.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class RootConfiguration {
 
     @Bean
-    public Calculator calculator() {
-        return new Calculator();
+    public Map<String, Operation> getOperation(Operation addition, Operation subtraction, Operation multiply, Operation divide) {
+        Map<String, Operation> operationMap = new HashMap<>();
+        operationMap.put("1", addition);
+        operationMap.put("2", subtraction);
+        operationMap.put("3", multiply);
+        operationMap.put("4", divide);
+        return operationMap;
+    }
+
+    @Bean
+    public Map<String, Command> getCommand(Command registration, Command authorization) {
+        Map<String, Command> commandMap = new HashMap<>();
+        commandMap.put("1", registration);
+        commandMap.put("2", authorization);
+        return commandMap;
+    }
+
+    @Bean
+    public CalculatorService calculatorService() {
+        return new CalculatorService();
+    }
+
+    @Bean
+    public UserService userService() {
+        return new UserService();
+    }
+
+    @Bean
+    public UserDao userDao() {
+        return InMemoryUserDao.getInstance();
+    }
+
+    @Bean
+    public HistoryOperationDao historyOperationDao() {
+        return InMemoryHistoryOperationDao.getInstance();
+    }
+
+    @Bean
+    public User user() {
+        return new User();
+    }
+
+    @Bean
+    public Operation addition() {
+        return new Addition();
+    }
+
+    @Bean
+    public Operation subtraction() {
+        return new Subtraction();
+    }
+
+    @Bean
+    public Operation divide() {
+        return new Divide();
+    }
+
+    @Bean
+    public Operation multiply() {
+        return new Multiply();
+    }
+
+    @Bean
+    public Command registration() {
+        return new Registration();
+    }
+
+    @Bean
+    public Command authorization() {
+        return new Authorization();
     }
 }
+
